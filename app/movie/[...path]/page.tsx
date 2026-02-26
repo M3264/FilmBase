aa2b840a03ac645d4e029cf49fbf9205fd698c88
Kenny <miracle32669@outlook.com>
2026-02-26 13:25:06 +0100

    Update page.tsx

--- app/movie/[...path]/page.tsx
+++ app/movie/[...path]/page.tsx
@@ -7,7 +7,7 @@ import Image from "next/image"
 import { Play } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
-const BASE_URL = "https://silver-fishstick-y2se.onrender.com"
+const BASE_URL = "https://silver-fishstic.onrender.com"
 
 export default async function MoviePage({
   params,
