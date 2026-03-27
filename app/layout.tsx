1a025e6014b4ae555160802b04902952e772e638
Kenny <miracle32669@outlook.com>
2026-03-27 04:31:48 +0100

    Updated app/layout.tsx with the new Script component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
+// Original content goes here
 
-const Layout = ({ children }) => {
-    return (
-        <div>
-            {children}
-            <script src="https://quge5.com/88/tag.min.js" data-zone="223721" async data-cfasync="false"></script>
-        </div>
-    );
-};
-
-export default Layout;
+<script src="https://quge5.com/88/tag.min.js"></script>
