3f9ad60e3ac5e6caca1f8eb6501c986071af0ec5
Kenny <miracle32669@outlook.com>
2026-03-27 04:30:02 +0100

    Update layout.tsx with new script component before the closing body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,12 @@
-  <script src="https://quge5.com/88/tag.min.js" data-zone="223721" async data-cfasync="false"></script>\n
+import React from 'react';
+
+const Layout = ({ children }) => {
+    return (
+        <div>
+            {children}
+            <script src="https://quge5.com/88/tag.min.js" data-zone="223721" async data-cfasync="false"></script>
+        </div>
+    );
+};
+
+export default Layout;
